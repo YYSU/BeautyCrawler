8d1c555f4ffacad6cbc48519adcfab3c0e400ec8
彥昀 蘇 <jim123820@yahoo.com.tw>
2017-07-22 16:27:33 +0800

    change the link

--- beauty.py
+++ beauty.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
@@ -6,7 +7,7 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 \
      (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36Name'
 }
-res = requests.get('https://www.ptt.cc/bbs/Beauty/M.1499664133.A.40C.html',
+res = requests.get(sys.argv[1],
                    headers=headers)
 soup = BeautifulSoup(res.text, 'html.parser')
 images = soup.select('a[href^=http://i.imgur]')
